f42a3fc3e619f1a2e0d8cd40f8d69297ec7c4740
amit-chinder <chinderamit@gmail.com>
2022-09-17 22:30:57 +0530

    java

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/82/40fca76ea536001d1a6aca6ea3ad6492
@@ -0,0 +1,9 @@
+
+public class sub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ sampl_project/src/sampl_project/class1.java
@@ -0,0 +1,11 @@
+package sampl_project;
+
+public class class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hello world!!");
+System.out.println("hello");
+	}
+
+}
